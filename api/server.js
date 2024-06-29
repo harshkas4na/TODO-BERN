bd277adc15c2c8196626fef4c95aff089ab607b2
Harsh kasana <bt22ece106@gmail.com>
2024-06-29 22:21:53 +0530

    add deploy scripts

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const cors = require("cors")
 const tasks=require('./routes/routes')
 const app = express();
+const path = require('path')
 
 //user-/api/ethereum/create-task -> server.js -> routes.js -> controller.js -> tasks.js
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname + '/public')))
+
 app.use('/api/ethereum',tasks)
 
 const PORT=3000;
